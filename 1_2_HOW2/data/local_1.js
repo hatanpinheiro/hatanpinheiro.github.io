feb2d9c2535d5fe7db6c770d51aaae483691e07a
Hatan Pinheiro Silva <53164475+hatanpinheiro@users.noreply.github.com>
2020-07-04 01:19:35 -0300

    Add files via upload

---
+++ 1_2_HOW2/data/local_1.js
@@ -0,0 +1,8 @@
+var json_local_1 = {
+"type": "FeatureCollection",
+"name": "local_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -48.505142431115026, -27.542278179712014 ] } }
+]
+}
